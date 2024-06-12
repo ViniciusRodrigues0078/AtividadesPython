3a9fc42c8d2d8eb6064c32055b267c0fbe72950b
ViniciusRodrigues0078 <vinicius.rodrigues.g.m.pereira@gmail.com>
2024-06-12 11:56:26 -0300

    Adicionada atividade sobre operações

---
+++ Atividade2.py
@@ -0,0 +1,10 @@
+Salario = 1765.43
+Porcentagem = 5
+Aumento = Salario * Porcentagem / 100
+print ("O aumento será de", Aumento)
+print ("A soma do salário com o aumento é igual a exatamente", Salario + Aumento)
+print ("Então, o salário é igual a = R$1853,70")
+print ("Ou")
+print (round (Salario + (Salario * Porcentagem / 100),2 ) )
+"""Observações
+- Usar o atributo round() para definir a quantidade máxima de casas decimais"""
