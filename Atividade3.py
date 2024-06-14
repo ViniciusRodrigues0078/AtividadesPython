153bff3d0f31f07b986d44b7ae866ef5a26a92c7
ViniciusRodrigues0078 <vinicius.rodrigues.g.m.pereira@gmail.com>
2024-06-14 14:01:08 -0300

    Atividade sobre conversão de tipos

---
+++ Atividade3.py
@@ -0,0 +1,14 @@
+"""a = 0
+b = 0
+a = input ("Digite um valor para a: ")
+b = input ("Digite um valor para a: ")
+print (a + b)"""
+
+"""c = 3
+d = 0
+d = input ("Digite um valor para d: ")
+e = "O valor dessa expressão é: ", c * d
+print (e)"""
+
+"""a = input ("Digite a sua idade: ")
+print (2024 - a)"""
